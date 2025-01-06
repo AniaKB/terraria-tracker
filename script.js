9a3521b89c39f427bca17d5b8f3bb86c2d9892cb
AniaKB <aniakbennett@gmail.com>
2025-01-05 19:32:09 -0500

    adding weapons and pics

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 weapons = [
   swords = [
     "copper shortsword" = {
-      pic = "",
-      material = ,
-      crafts = [],
-      hardmode = false,
-    }
+      pic: "photos/weapons/Copper_Shortsword.png",
+      ingredients: [ "copper bar" x5 ],
+      station: "iron anvil" || "lead anvil",
+      material: true,
+      crafts: [ zenith ],
+      hardmode: false,
+    },
+    "boreal wood sword" = {
+      pic: "photos/weapons/Boreal_Wood_Sword.png",
+      ingredients: [],
+      station: "iron anvil" || "lead anvil",
+      material: false,
+      crafts: null,
+      hardmode: false,
+    },
   ],
   yoyos = [
 
