ea62615c1cff53aa4b452d83d2e0d0ed4b25ab92
unknown <aniakbennett@gmail.com>
2025-01-12 18:14:38 -0500

    adding more data and fleshing out layout for tinker page

--- script.js
+++ script.js
@@ -1,34 +1,54 @@
-weapons = {
+/* export const data = { */
+
+items = {
+  raw: [
+    {
+    id: 1,
+    name: "copper ore", 
+    pic: "copper-ore.png",
+    ingredients: [],
+    station: null,
+    material: true,
+    hardmode: false,
+    },
+    {
+      id: 2,
+      name: "copper bar", 
+      pic: "copper-bar.png",
+      ingredients: [ 1, ],
+      station: null,
+      material: true,
+      hardmode: false,
+      }
+  ],
   swords: [
     {
+      id: 20,
       name: "copper shortsword", 
-      pic: "photos/weapons/Copper_Shortsword.png",
-      ingredients: {
-        num: 5, 
-        ingredient:"copper bar" },
+      pic: "photos/Copper_Shortsword.png",
+      ingredients: [],
       station: "iron anvil" || "lead anvil",
       material: true,
-      crafts: [],
       hardmode: false,
     },
     {
+      id: 20,
       name: "boreal wood sword",
-      pic: "photos/weapons/Boreal_Wood_Sword.png",
+      pic: "photos/Boreal_Wood_Sword.png",
       ingredients: [],
       station: "iron anvil" || "lead anvil",
       material: false,
-      crafts: null,
       hardmode: false,
     }
   ],
   yoyos: [
     {
+      id: 20,
       name: "wooden yoyo",
-      pic: "photos/weapons/Wooden_Yoyo.png",
-      ingredients: [ "wood"],
+      pic: "photos/Wooden_Yoyo.png",
+      ingredients: [ "wood" ],
       station: "iron anvil" || "lead anvil",
       material: false,
-      crafts: null,
       hardmode: false,
     }
    ] /*
@@ -42,10 +62,10 @@ weapons = {
 const container = document.getElementById("container");
 
 const header = document.createElement("h2");
-header.textContent = weapons[0];
+header.textContent = items[0];
 container.appendChild(header);
 
-weapons.swords.forEach(item => {
+items.swords.forEach(item => {
     const div = document.createElement("div");
 
     const text = document.createElement("p");
